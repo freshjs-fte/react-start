4401e1f4376d8dd74204cd48114a19c92573848f
Yevhenii <freshjs.fte@gmail.com>
2021-09-30 21:13:36 +0300

    Add pagination and refactor for api method

--- src/components/WantedList/index.jsx
+++ src/components/WantedList/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { getRandomUser } from "../../api";
 
 class WantedList extends Component {
   constructor(props) {
@@ -6,28 +7,54 @@ class WantedList extends Component {
 
     this.state = {
       list: [],
+      page: 1,
     };
   }
 
   componentDidMount() {
-    fetch("https://randomuser.me/api/?results=5" /*?seed=wanted&"*/)
-      .then((response) => {
-        return response.json();
-      })
-
-      .then((data) => {
-        console.log(data);
-        const wanted = data.results;
-
-        this.setState({
-          list: wanted,
-        });
-      });
+    this.load();
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.page !== prevState.page) {
+      this.load();
+    }
+  }
+
+  load = () => {
+    getRandomUser({
+      results: 5,
+      seed: "wanted",
+      page: this.state.page,
+      nat: ["us", "fr"],
+    })
+    .then((data) => {
+      console.log(data);
+      const wanted = data.results;
+
+      this.setState({
+        list: wanted,
+      });
+    });
+  };
+
+  nextPage = () => {
+    this.setState((prevState) => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  prevPage = () => {
+    this.setState((prevState) => ({
+      page: prevState.page - 1,
+    }));
+  };
 
   render() {
     return (
       <div>
+        <button onClick={this.prevPage}>Prev page</button>
+        <button onClick={this.nextPage}>Next page</button>
         <ul>
           {this.state.list.map((elem, index) => {
             return (
@@ -41,5 +68,20 @@ class WantedList extends Component {
     );
   }
 }
+
+/* 
+    set state queue
+    initial state = {test: 1}
+
+    1. setState( {test: ...} )
+    {test: this.state+1}, {test: this.state+1 }
+    result state = {test: 2}
+
+    2. setState( (prevState) => { return {test: prevState...} } )
+    (prevState) => {test: prevState+1}, (prevState) => {test: prevState+1 }
+    result state = {test: 3}
+
+    
+*/
 
 export default WantedList;
