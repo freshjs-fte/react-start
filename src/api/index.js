bf3965dde4d4617b8ec04da4bb480cd090afb267
Yevhenii <freshjs.fte@gmail.com>
2021-09-30 21:13:24 +0300

    Add api method

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+import queryString from "query-string";
+
+const randomUserAPI = "https://randomuser.me/api/";
+
+/**
+ *
+ * @param {Object} options
+ * @param {number} [options.results]
+ * @param {string} [options.seed]
+ * @param {number} [options.page]
+ * @returns {Promise} users
+ */
+export function getRandomUser(options) {
+  console.log(
+    queryString.stringify(options, {
+      arrayFormat: "comma",
+    })
+  );
+
+  return fetch(
+    `${randomUserAPI}?${queryString.stringify(options, {
+      arrayFormat: "comma",
+    })}`
+  ).then((response) => {
+    return response.json();
+  });
+}
