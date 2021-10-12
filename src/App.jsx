68276fda9f78f6245eb0343af147e299248886f8
Yevhenii <freshjs.fte@gmail.com>
2021-10-12 17:44:01 +0300

    Add useCallback and useMemo examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import Main from "./components/Main";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
-// import TaskList from "./components/TaskList";
-// import WantedList from "./components/WantedList";
-// import CounterPage from "./pages/Counter";
-// import NotFoundPage from "./pages/NotFound";
-
-import Tree from "./components/Tree";
-
-import { UserContext, ThemeContext } from "./contexts";
+import Main from "./components/Main";
+import { ThemeContext } from "./contexts";
 
 import "./App.css";
-import StateHook from "./hooks/state";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+function App() {
+  const [show, setShow] = useState(true);
 
-    this.state = {
-      user: {
-        firstName: "John",
-        lastName: "Silverhand",
-        age: 44,
-        email: "example@mail.com",
-      },
-      theme: "DARK",
-    };
-  }
+  const themeState = useState("LIGHT");
 
-  render() {
-    return (
-      <UserContext.Provider value={this.state.user}>
-        <ThemeContext.Provider value={this.state.theme}>
-          <Tree />
-        </ThemeContext.Provider>
-      </UserContext.Provider>
-    );
-  }
+  return (
+    <ThemeContext.Provider value={themeState}>
+      <button
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        Show/Hide
+      </button>
+
+      <Main show={show}></Main>
+      
+    </ThemeContext.Provider>
+  );
 }
 
 export default App;
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,56 @@
+import { useCallback, useContext, useEffect, useMemo, useState } from "react";
+import { ThemeContext } from "../../contexts";
+import styles from "./Main.module.scss";
+
+const arrayImages = [
+  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+  "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg",
+  "https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270",
+];
+
 function Main(props) {
+  const [theme, setTheme] = useContext(ThemeContext);
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  // запускает
+  useEffect(() => {});
+
+  // создает
+  const toggleThemeFunc = useCallback(() => {
+    console.log('theme');
+    if (theme === "LIGHT") {
+      setTheme("DARK");
+    } else {
+      setTheme("LIGHT");
+    }
+  }, [theme, setTheme]);
+
+  // useMemo запускает
+  const themeStyles = useMemo(() => styles[theme], [theme]);
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % arrayImages.length);
+
+      console.log(currentSlide);
+    }, 2000);
+
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, [currentSlide]);
+
+  useEffect(() => {
+    console.log('setCurrentSlide changed');
+  }, [setCurrentSlide])
+
   return (
-    <div>
-      HOME
+    <div className={themeStyles}>
       <div>
-        {props.userData.firstName} {props.userData.lastName}
+        <img src={arrayImages[currentSlide]} alt="" />
       </div>
+      HOME
+      <button onClick={toggleThemeFunc}>Toggle theme</button>
     </div>
   );
 }
