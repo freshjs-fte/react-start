bdc4364aa5fb63e7d6aec3218e13feea866e9c69
Yevhenii <freshjs.fte@gmail.com>
2021-10-13 21:07:25 +0300

    Add logo link

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import RegisterForm from "../../components/forms/RegisterForm";
 
 export default function SignUpPage() {
@@ -10,6 +11,9 @@ export default function SignUpPage() {
 
   return (
     <section>
+      <Link to="/" style={{ backgroundColor: "#555", display: "inline-block" }}>
+        <img src="/images/logo.png" alt="LOGO" style={{ display: "block" }} />
+      </Link>
       <h1>CREATE AN ACCOUNT</h1>
       <h2>We always keep your name and email address private.</h2>
       <RegisterForm onSubmit={onSubmit} />
