b3f28079da0d60cc8d583fa8509590c710af16fe
Yevhenii <freshjs.fte@gmail.com>
2021-10-15 19:33:10 +0300

    Add MyInput

--- src/components/forms/MyInput/index.jsx
+++ src/components/forms/MyInput/index.jsx
@@ -1,13 +1,21 @@
-import { ErrorMessage, Field } from "formik";
-// classnames
 import React from "react";
+import { ErrorMessage, Field } from "formik";
+import cn from "classnames";
+import styles from "./MyInput.module.sass";
 
-export default function MyInput({ name, errors, touched, ...rest }) {
-    
+export default function MyInput({ name, errors, touched,values,  ...rest }) {
+  const classNames = cn(styles.input, {
+    [styles.valid]: touched[name] && !errors[name],
+    [styles.invalid]: touched[name] && errors[name],
+  });
+
   return (
-    <label>
-      <Field  name={name} {...rest} />
-      <ErrorMessage name={name} />
-    </label>
+    false,
+    (
+      <label>
+        <Field name={name} {...rest} className={classNames} />
+        <ErrorMessage name={name} className={styles.errorMsg} />
+      </label>
+    )
   );
 }
